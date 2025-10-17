d88413ca2d2b76b8a2d4409364c2296087fdf596
kavya24ss <sagikavyasri@gmail.com>
2025-10-17 20:54:14 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,15 @@
+def add(a,b):
+  return a+b
+def subtract(a,b):
+  return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+if __name__=="__main__":
+   x=10
+   y=5
+print("Addition:",add(x,y))
+print("subtraction:",subtract(x,y))
+print("multiplication:",multiply(x,y))
+print("division:",divide(x,y))
